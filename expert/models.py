7eefc6b2a9fa7a173c12ace68180db242beec3e3
mehmet <mehmet@edgle.com>
2020-03-31 10:47:06 -0500

    make modified field of Expert model aware of time zone

--- expert/models.py
+++ expert/models.py
@@ -2,9 +2,11 @@ import datetime
 
 from django.conf import settings
 from django.db import models
+from django.utils.timezone import make_aware
 
 
 class Expert(models.Model):
+    # user editable fields
     name = models.CharField(max_length=100, null=False, blank=False)
     friends = models.ManyToManyField('self')
     long_url = models.URLField(verbose_name='URL', null=False, blank=False)
@@ -18,7 +20,7 @@ class Expert(models.Model):
     def save(self, *args, **kwargs):
         original = None
         if self.pk is not None:  # being updated
-            self.modified = datetime.datetime.now
+            self.modified = make_aware(datetime.datetime.now())
             original = Expert.objects.get(pk=self.pk)
         
         # shorten url
