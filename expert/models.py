7abff2fda7d012ebc141b19773b03f1a312babf6
mehmet <mehmet@edgle.com>
2020-03-31 09:20:28 -0500

    expert model

--- expert/models.py
+++ expert/models.py
@@ -1,3 +1,31 @@
+import datetime
+
 from django.db import models
 
-# Create your models here.
+
+class Expert(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    friends = models.ManyToManyField('self')
+    url = models.URLField(null=False, blank=False)
+    # non user editable fields
+    url_short = models.URLField(null=True, blank=True)
+    heading_text = models.TextField(null=True, blank=True)
+    # there is no need for related_name because friendships are bi-directional
+    created = models.DateTimeField(default=datetime.datetime.now)
+    modified = models.DateTimeField(default=None)
+    
+    def save(self, *args, **kwargs):
+        original = None
+        if self.pk is not None:  # being updated
+            self.modified = datetime.datetime.now
+            original = Expert.objects.get(pk=self.pk)
+        
+        # shorten url
+        if original is None or self.url != original.url:
+            pass
+        
+        # get heading text
+        if original is None or self.url != original.url:
+            pass
+        
+        super().save(*args, **kwargs)
